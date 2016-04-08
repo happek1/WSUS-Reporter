28e6bec45707faa0f46ba6a9df4d82944f7bfbeb
happek1 <happek@gmail.com>
2016-04-07 20:03:39 -0400

    Added files via upload

---
+++ getgroups.ps1
@@ -0,0 +1,9 @@
+﻿## Retreives WSUS Groups and dumps into a text file ##
+
+[reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration") 
+ 
+$wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::getUpdateServer() 
+foreach( $group in $wsus.GetComputerTargetGroups() ) 
+{ 
+    $group.Name | out-file groups.txt -Append
+}
