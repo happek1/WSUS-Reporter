8964d84df336949ffeea877aa88f2954013099b4
happek1 <happek@gmail.com>
2016-04-07 20:06:43 -0400

    Added files via upload

---
+++ emailreports.ps1
@@ -0,0 +1,8 @@
+﻿####Sends monthly WSUS reports to the respective teams ####
+###WSUS reports generated must use the exact names in the containing folder or it will not find the attachement####
+Cd .\reports
+$Server = "mail.server.com"
+
+#Send to Group Owner - Sends email report to groupDL@domain.com
+$bodyADA = Get-Content "Group.html" -raw
+send-mailmessage -to groupDL@domain.com -from noc@reedsmith.com -CC noc@reedsmith.com -subject "WSUS Report for Group" -body $bodyADA -BodyAsHtml -Attachments "Group.html" -smtpServer $Server
